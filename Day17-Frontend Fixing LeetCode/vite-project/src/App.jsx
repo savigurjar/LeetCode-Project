738e4ae56cc7da6ddf46e2fa409434cef3970b08
savigurjar <savigurjar1201@gmail.com>
2025-10-14 14:15:27 +0530

    problem submit remain

---
+++ Day17-Frontend Fixing LeetCode/vite-project/src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { checkAuth } from "./features/authSlice";
+import AdminPanel from "./pages/AdminPanel";
+import ProblemPage from "./pages/ProblemPage";
+function App(){
+  
+  const dispatch = useDispatch();
+  const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
+
+  // check initial authentication
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+
+  
+  if (loading) {
+    return <div className="min-h-screen flex items-center justify-center">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>;
+  }
+
+  return(
+  <>
+    <Routes>
+      <Route path="/" element={isAuthenticated ?<Home/>:<Navigate to="/signup" />}></Route>
+      <Route path="/login" element={isAuthenticated?<Navigate to="/" />:<Login></Login>}></Route>
+      <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
+      <Route path="/admin" element={<AdminPanel/>}></Route>
+      <Route path="/problem/:problemId" element={<ProblemPage/>}></Route>
+      {/* <Route 
+        path="/admin" 
+        element={
+          isAuthenticated && user?.role === 'admin' ? 
+            <AdminPanel /> : 
+            <Navigate to="/" />
+        } 
+      /> */}
+    </Routes>
+  </>
+  )
+}
+
+export default App;
